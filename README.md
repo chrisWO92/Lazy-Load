d94f41f6902e84afabc296d9abf1d5aae6048a2b
cristian <chrisworkingonline@gmail.com>
2022-02-18 19:10:26 -0300

    final commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Lazy-Load
-Using InterserctionObserver and Fetch Async Await Request
+Using InterserctionObserver and Fetch Async Await Request for loading div elements with scrolling
--- app.js
+++ app.js
@@ -1,12 +1,65 @@
 "use strict";
 
-const getInfo = async () => {
-    let peticion = await fetch("informacion.txt");
-    let resultado = await peticion.json();
-    return resultado;
+const noHayMasPublis = document.createElement("DIV");
+noHayMasPublis.classList.add("nohaymaspublis");
+noHayMasPublis.textContent = "No hay más publicaciones";
+
+const creatorPub = (namePub, contentPub) => {
+    const publication = document.createElement("DIV");
+    const name = document.createElement("H2");
+    const content = document.createElement("P");
+    const comments = document.createElement("DIV");
+    const comment = document.createElement("INPUT");
+    const submit = document.createElement("INPUT");
+
+    submit.type = "submit";
+
+    comments.appendChild(comment);
+    comments.appendChild(submit);
+
+    publication.appendChild(name);
+    publication.appendChild(content);
+    publication.appendChild(comments);
+
+    publication.classList.add("publication");
+    name.classList.add("name");
+    comments.classList.add("comments");
+    comment.classList.add("comment");
+    submit.classList.add("submit");
+
+    name.textContent = namePub;
+    content.textContent = contentPub;
+    
+    return publication;
 }
 
-let res = getInfo().then(res => {return res.content});
-console.log(res);
+const verifyVisibility = (entry) => {
+    if (entry[0].isIntersecting) cargarPublis(3);
+}
 
-//console.log(getInfo().then(res => res.content[0]));
+const observer = new IntersectionObserver(verifyVisibility);
+
+let contador = 0;
+
+const cargarPublis = async (num) => {
+    let peticion = await fetch("informacion.txt");
+    let resultadoJSON = await peticion.json();
+    let content = resultadoJSON.content;
+
+    for (let i = 0; i < num; i++) {
+        if (contador < content.length) {
+            let publicacion = creatorPub(content[contador].nombre, content[contador].contenido);
+            document.body.appendChild(publicacion);
+            if (i == num - 1){
+                observer.observe(publicacion);
+            }
+            contador ++;
+        }else if (contador == content.length){
+            document.body.appendChild(noHayMasPublis);
+            break;
+        }
+    }
+   
+}
+
+cargarPublis(4);
